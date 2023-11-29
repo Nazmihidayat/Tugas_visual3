072ca6a1367f5f257b2c9fdd2163349d9b13ab38
Nazmihidayat <133829113+Nazmihidayat@users.noreply.github.com>
2023-11-29 12:51:21 +0800

    form login dan table anggota

---
+++ delphi_Tugas 5/Unit2.dfm
@@ -0,0 +1,92 @@
+object Form2: TForm2
+  Left = 565
+  Top = 244
+  Width = 446
+  Height = 261
+  Caption = 'Form2'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object lbl1: TLabel
+    Left = 56
+    Top = 32
+    Width = 41
+    Height = 13
+    Caption = 'Anggota'
+  end
+  object lbl2: TLabel
+    Left = 152
+    Top = 32
+    Width = 23
+    Height = 13
+    Caption = 'Buku'
+  end
+  object lbl3: TLabel
+    Left = 240
+    Top = 32
+    Width = 105
+    Height = 13
+    Caption = 'Buku Setelah Dipinjam'
+  end
+  object lbl4: TLabel
+    Left = 212
+    Top = 108
+    Width = 57
+    Height = 13
+    Caption = 'Peminjaman'
+  end
+  object lbl5: TLabel
+    Left = 112
+    Top = 108
+    Width = 18
+    Height = 13
+    Caption = 'Rak'
+  end
+  object btn1: TButton
+    Left = 40
+    Top = 60
+    Width = 75
+    Height = 25
+    Caption = 'Anggota'
+    TabOrder = 0
+    OnClick = btn1Click
+  end
+  object btn2: TButton
+    Left = 132
+    Top = 60
+    Width = 75
+    Height = 25
+    Caption = 'Buku'
+    TabOrder = 1
+  end
+  object btn3: TButton
+    Left = 232
+    Top = 60
+    Width = 125
+    Height = 25
+    Caption = 'Buku Setelah Dipinjam'
+    TabOrder = 2
+  end
+  object btn4: TButton
+    Left = 84
+    Top = 132
+    Width = 75
+    Height = 25
+    Caption = 'Rak'
+    TabOrder = 3
+  end
+  object btn5: TButton
+    Left = 204
+    Top = 132
+    Width = 75
+    Height = 25
+    Caption = 'Peminjaman'
+    TabOrder = 4
+  end
+end
---
+++ delphi_Tugas 5/db_perpustakaan2.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 29 Nov 2023 pada 04.42
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_perpustakaan2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id_anggota` int(10) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `no_telepon` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `anggota`
+--
+
+INSERT INTO `anggota` (`id_anggota`, `nama`, `alamat`, `no_telepon`) VALUES
+(1, 'as', 'asa', '0868687');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(10) NOT NULL,
+  `id_rak` int(10) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `pengarang` varchar(50) NOT NULL,
+  `tahun_terbit` varchar(10) NOT NULL,
+  `jumlah_stok` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku_setelah_dipinjam`
+--
+
+CREATE TABLE `buku_setelah_dipinjam` (
+  `id_buku_setalah` int(10) NOT NULL,
+  `id_peminjaman` int(10) NOT NULL,
+  `id_buku` int(10) NOT NULL,
+  `kondisi_buku` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` int(10) NOT NULL,
+  `id_buku` int(10) NOT NULL,
+  `id_anggota` int(10) NOT NULL,
+  `tanggal_peminjaman` varchar(10) NOT NULL,
+  `tanggal_pengembalian` varchar(10) NOT NULL,
+  `status_pengembalian` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rak`
+--
+
+CREATE TABLE `rak` (
+  `id_rak` int(10) NOT NULL,
+  `nama_rak` varchar(15) NOT NULL,
+  `lokasi` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(5) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `level` char(10) DEFAULT NULL,
+  `status` char(10) DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  `created_userid` int(10) DEFAULT NULL,
+  `update_at` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `level`, `status`, `created_at`, `created_userid`, `update_at`) VALUES
+(1, 'admin', 'admin', 'admin', 'true', '2023-11-01', 1, '2023-11-02');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `buku_setelah_dipinjam`
+--
+ALTER TABLE `buku_setelah_dipinjam`
+  ADD PRIMARY KEY (`id_buku_setalah`),
+  ADD KEY `id_peminjaman` (`id_peminjaman`,`id_buku`),
+  ADD KEY `id_buku` (`id_buku`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`);
+
+--
+-- Indeks untuk tabel `rak`
+--
+ALTER TABLE `rak`
+  ADD PRIMARY KEY (`id_rak`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `anggota`
+--
+ALTER TABLE `anggota`
+  MODIFY `id_anggota` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
